2a154360664c5f3c7900ee3fc571034741df3ede
Alex <64538834+alex-safonov@users.noreply.github.com>
2022-05-24 18:49:37 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+ <div id="top__img_urgant"></div>
+						
+<script type="text/javascript">
+  if(window.innerWidth < 1024) {
+      document.getElementById("top__img_urgant").innerHTML = '<div class="top__img top__img-mobile visible-xs"><img src="/static/build/new_main/img/urgant_mobile.png"/></div>'
+  } else {
+      document.getElementById("top__img_urgant").innerHTML = '<div class="top__img hidden-xs"><img src="/static/build/new_main/img/urgant.png"/></div>'
+  }
+</script>
